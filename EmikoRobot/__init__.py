a40dfc297376eb08a98f65d3c7e742d8c3731f88
IONBOT <113283570+ionmusic@users.noreply.github.com>
2022-10-20 18:50:49 +0700

    Update __init__.py

--- EmikoRobot/__init__.py
+++ EmikoRobot/__init__.py
@@ -40,7 +40,7 @@ logging.getLogger('ptbcontrib.postgres_persistence.postgrespersistence').setLeve
 LOGGER = logging.getLogger('[EmikoRobot]')
 LOGGER.info("Emiko is starting. | An Kennedy Project Parts. | Licensed under GPLv3.")
 LOGGER.info("Not affiliated to other anime or Villain in any way whatsoever.")
-LOGGER.info("Project maintained by: github.com/iamuput (t.me/iamuput)")
+LOGGER.info("Project maintained by: github.com/ionmusic (t.me/disinikazu)")
 
 # if version < 3.9, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 8:
