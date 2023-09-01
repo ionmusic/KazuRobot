b7b269b1cf836bd7708b5d8388695517d518702f
Sourcery AI <>
2023-09-01 13:23:12 +0000

    'Refactored by Sourcery'

--- EmikoRobot/config.py
+++ EmikoRobot/config.py
@@ -3,7 +3,7 @@ import json
 import os
 
 def get_user_list(config, key):
-    with open("{}/EmikoRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open(f"{os.getcwd()}/EmikoRobot/{config}", "r") as json_file:
         return json.load(json_file)[key]
 
 # Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
